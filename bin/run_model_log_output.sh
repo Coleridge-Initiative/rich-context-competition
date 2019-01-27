7291be845f0ea170d5ced7f46d5a323e776b2fbc
Jonathan Morgan <jsm296@nyu.edu>
2019-01-27 03:24:26 +0000

    put enhancements back that got whacked in a previous commit, added hyphen to shell script log output file name.

--- bin/run_model_log_output.sh
+++ bin/run_model_log_output.sh
@@ -3,4 +3,4 @@
 # Open code block, output of which we will redirect to a file.
 {
     time ./rcc.sh run-stop
-} 2>&1 | tee -a model_run_output$(date +"%Y%m%d%H%M%S").txt
+} 2>&1 | tee -a model_run_output-$(date +"%Y%m%d%H%M%S").txt
